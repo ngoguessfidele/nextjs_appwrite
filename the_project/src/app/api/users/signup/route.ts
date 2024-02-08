cca4999ef1fbb604a032c70d607518a06bbafd2c
ngoguessfidele <ngoguessfidele@gmail.com>
2024-02-08 09:02:50 +0200

    more functionalitities

--- the_project/src/app/api/users/signup/route.ts
+++ the_project/src/app/api/users/signup/route.ts
@@ -1,7 +1,8 @@
-import {connect} from "@/dbConfig/dbConfi";
+import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 
 connect()
@@ -31,6 +32,10 @@ export async function POST(request: NextRequest) {
 
         const savedUser = await newUser.save()
         console.log(savedUser)
+
+        //send verification email
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
             message: "User created successfully",
